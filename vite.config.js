d99fb285761d4a60002a0d7c5ab59b8a29a5a7bf
danieleprice123 <dan.tofu.price@gmail.com>
2025-09-03 07:43:42 -0700

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    legacy({
+      targets: ['ie >= 11'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+    })
+  ],
+  build: {
+    // ensure relative asset paths work inside the widget zip
+    assetsDir: 'assets'
+  }
+})
